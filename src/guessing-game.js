c8b8bbce189893f27d9df6b976ee988da3ad9b90
Yuliya Laptsinskaya <89513166+Kroshka0001@users.noreply.github.com>
2023-02-28 02:39:32 +0300

    feat: update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        let currentGuess = null
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        this.currentGuess = this.min + Math.ceil((this.max - this.min) / 2)
+        return this.currentGuess
     }
 
     lower() {
-
+        this.max = this.currentGuess
+        this.setRange(this.min, this.max)
     }
 
     greater() {
-
+        this.min = this.currentGuess
+        this.setRange(this.min, this.max)
     }
 }
 
